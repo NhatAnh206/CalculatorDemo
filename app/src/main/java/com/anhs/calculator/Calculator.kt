98febcdd5026b421eb01d451eca533614ceecdd2
anh lanh <anhntn62@wru.vn>
2020-01-08 10:28:24 +0700

    Fix comment

--- app/src/main/java/com/anhs/calculator/Calculator.kt
+++ app/src/main/java/com/anhs/calculator/Calculator.kt
@@ -1,4 +1,4 @@
-package com.example.caculator
+package com.anhs.calculator
 
 
 import android.os.Bundle
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.anhs.calculator.R
 import kotlinx.android.synthetic.main.fragment_calculator.*
 
 /**
@@ -15,14 +14,14 @@ import kotlinx.android.synthetic.main.fragment_calculator.*
 class Calculator : Fragment(), View.OnClickListener {
 
 
-    var input1 = 0.0
-    var input2 = 0.0
+    private var input1 = 0.0
+    private var input2 = 0.0
 
-    var sum = false
-    var sub = false
-    var mul = false
-    var div = false
-    var per = false
+    private var sum = false
+    private var sub = false
+    private var mul = false
+    private var div = false
+    private var per = false
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -36,121 +35,121 @@ class Calculator : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initView()
+        setListener()
     }
 
-    private fun initView() {
-        bt_0.setOnClickListener(this)
-        bt_1.setOnClickListener(this)
-        bt_2.setOnClickListener(this)
-        bt_3.setOnClickListener(this)
-        bt_4.setOnClickListener(this)
-        bt_5.setOnClickListener(this)
-        bt_6.setOnClickListener(this)
-        bt_7.setOnClickListener(this)
-        bt_8.setOnClickListener(this)
-        bt_9.setOnClickListener(this)
-        bt_dot.setOnClickListener(this)
-        bt_sum.setOnClickListener(this)
-        bt_sub.setOnClickListener(this)
-        bt_mul.setOnClickListener(this)
-        bt_div.setOnClickListener(this)
-        bt_per.setOnClickListener(this)
-        bt_ac.setOnClickListener(this)
-        bt_c.setOnClickListener(this)
-        bt_result.setOnClickListener(this)
+    private fun setListener() {
+        buttonZero.setOnClickListener(this)
+        buttonOne.setOnClickListener(this)
+        buttonTwo.setOnClickListener(this)
+        buttonThree.setOnClickListener(this)
+        buttonFour.setOnClickListener(this)
+        buttonFive.setOnClickListener(this)
+        buttonSix.setOnClickListener(this)
+        buttonSeven.setOnClickListener(this)
+        buttonEight.setOnClickListener(this)
+        buttonNine.setOnClickListener(this)
+        buttonDot.setOnClickListener(this)
+        buttonSum.setOnClickListener(this)
+        buttonSubtract.setOnClickListener(this)
+        buttonMultipli.setOnClickListener(this)
+        buttonDivide.setOnClickListener(this)
+        buttonPercent.setOnClickListener(this)
+        buttonAc.setOnClickListener(this)
+        buttonC.setOnClickListener(this)
+        buttonEqual.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
-            R.id.bt_0 -> {
+            R.id.buttonZero -> {
                 enterNumber("0")
             }
-            R.id.bt_1 -> {
+            R.id.buttonOne -> {
                 enterNumber("1")
             }
-            R.id.bt_2 -> {
+            R.id.buttonTwo -> {
                 enterNumber("2")
             }
-            R.id.bt_3 -> {
+            R.id.buttonThree -> {
                 enterNumber("3")
             }
-            R.id.bt_4 -> {
+            R.id.buttonFour -> {
                 enterNumber("4")
             }
-            R.id.bt_5 -> {
+            R.id.buttonFive -> {
                 enterNumber("5")
             }
-            R.id.bt_6 -> {
+            R.id.buttonSix -> {
                 enterNumber("6")
             }
-            R.id.bt_7 -> {
+            R.id.buttonSeven -> {
                 enterNumber("7")
             }
-            R.id.bt_8 -> {
+            R.id.buttonEight -> {
                 enterNumber("8")
             }
-            R.id.bt_9 -> {
+            R.id.buttonNine -> {
                 enterNumber("9")
             }
-            R.id.bt_dot -> {
+            R.id.buttonDot -> {
                 enterNumber(",")
             }
-            R.id.bt_sum -> {
+            R.id.buttonSum -> {
                 sum = true
-                input1 = tv_result.text.toString().toDouble()
-                tv_result.text = "0"
+                input1 = textResult.text.toString().toDouble()
+                textResult.text = "0"
             }
-            R.id.bt_sub -> {
+            R.id.buttonSubtract -> {
                 sub = true
-                input1 = tv_result.text.toString().toDouble()
-                tv_result.text = "0"
+                input1 = textResult.text.toString().toDouble()
+                textResult.text = "0"
             }
-            R.id.bt_mul -> {
+            R.id.buttonMultipli -> {
                 mul = true
-                input1 = tv_result.text.toString().toDouble()
-                tv_result.text = "0"
+                input1 = textResult.text.toString().toDouble()
+                textResult.text = "0"
             }
-            R.id.bt_div -> {
+            R.id.buttonDivide -> {
                 div = true
-                input1 = tv_result.text.toString().toDouble()
-                tv_result.text = "0"
+                input1 = textResult.text.toString().toDouble()
+                textResult.text = "0"
             }
-            R.id.bt_per -> {
+            R.id.buttonPercent -> {
                 per = true
-                input1 = tv_result.text.toString().toDouble()
-                tv_result.text = "0"
+                input1 = textResult.text.toString().toDouble()
+                textResult.text = "0"
             }
-            R.id.bt_ac -> {
+            R.id.buttonAc -> {
                 val ac = 0
-                tv_result.text = ac.toString()
+                textResult.text = ac.toString()
             }
-            R.id.bt_c -> {
-                var number = tv_result.text.toString().length
+            R.id.buttonC -> {
+                val number = textResult.text.toString().length
 
-                var delete = tv_result.text.toString().substring(0, number - 1)
-                tv_result.text = delete
+                val delete = textResult.text.toString().substring(0, number - 1)
+                textResult.text = delete
             }
-            R.id.bt_result -> {
+            R.id.buttonEqual -> {
                 if (sum) {
-                    input2 = tv_result.text.toString().toDouble()
-                    tv_result.text = (input1 + input2).toString()
+                    input2 = textResult.text.toString().toDouble()
+                    textResult.text = (input1 + input2).toString()
                     sum = false
                 } else if (sub) {
-                    input2 = tv_result.text.toString().toDouble()
-                    tv_result.text = (input1 - input2).toString()
+                    input2 = textResult.text.toString().toDouble()
+                    textResult.text = (input1 - input2).toString()
                     sub = false
                 } else if (mul) {
-                    input2 = tv_result.text.toString().toDouble()
-                    tv_result.text = (input1 * input2).toString()
+                    input2 = textResult.text.toString().toDouble()
+                    textResult.text = (input1 * input2).toString()
                     mul = false
                 } else if (div) {
-                    input2 = tv_result.text.toString().toDouble()
-                    tv_result.text = (input1 / input2).toString()
+                    input2 = textResult.text.toString().toDouble()
+                    textResult.text = (input1 / input2).toString()
                     div = false
                 } else if (per) {
-                    input2 = tv_result.text.toString().toDouble()
-                    tv_result.text = (input1 * input2 / 100).toString()
+                    input2 = textResult.text.toString().toDouble()
+                    textResult.text = (input1 * input2 / 100).toString()
                     per = false
                 }
             }
@@ -158,7 +157,7 @@ class Calculator : Fragment(), View.OnClickListener {
     }
 
     private fun enterNumber(s: String) {
-        tv_result.text = tv_result.text.toString() + s
+        textResult.text = textResult.text.toString() + s
     }
 
 }
--- app/src/main/java/com/anhs/calculator/MainActivity.kt
+++ app/src/main/java/com/anhs/calculator/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.anhs.calculator
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.caculator.Calculator
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val frg= Calculator()
-        val tran = supportFragmentManager.beginTransaction()
-        tran.add(R.id.frg_cal, frg)
-        tran.commit()
+        val fragmentCalculator = Calculator()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragmentCalculator, fragmentCalculator)
+        transaction.commit()
     }
 }
